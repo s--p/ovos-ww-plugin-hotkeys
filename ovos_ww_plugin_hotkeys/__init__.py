23c6c35dca4eabc96f2268c51e7f22b173baf18d
timonvanhasselt <timonvanhasselt@users.noreply.github.com>
2023-08-07 16:49:44 +0200

    Update __init__.py

--- ovos_ww_plugin_hotkeys/__init__.py
+++ ovos_ww_plugin_hotkeys/__init__.py
@@ -2,12 +2,12 @@ from ovos_plugin_manager.templates.hotwords import HotWordEngine
 from evdev import InputDevice, ecodes, categorize
 
 class HotKeysWakeWordPlugin(HotWordEngine):
-    """Keyboard hotkeys, define key combo to trigger listening"""
+    """Spacebar hotkey, trigger listening with the spacebar"""
 
     def __init__(self, hotword="hotkeys", config=None, lang="en-us"):
         super().__init__(hotword, config or {}, lang)
         self.found_ww = False
-        self.hotkey_combo = self.config.get("hotkey", "space")
+        self.hotkey_code = ecodes.KEY_SPACE
         self.device = InputDevice("/dev/input/event6")
 
     def handle_hotkey_press(self):
@@ -28,15 +28,13 @@ class HotKeysWakeWordPlugin(HotWordEngine):
         found_wake_word method """
         # Read input events from the device
         for event in self.device.read():
-            if event.type == ecodes.EV_KEY:
-                if(event.code == KEY_SPACE and event.value == 1 ):
-                    LOG.info("space pressed")
-                    self.handle_hotkey_press()
-                
+            if event.type == ecodes.EV_KEY and event.code == self.hotkey_code and event.value == 1:
+                self.handle_hotkey_press()
+
     def stop(self):
         """ Perform any actions needed to shut down the hot word engine.
 
             This may include things such as unload loaded data or shutdown
             external processes.
         """
-        pass  
+        pass 
